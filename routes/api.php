94054ea0d21e0c8e08e0f4120b8af18d3f754e56
vedrosuwandi <43366004+vedrosuwandi@users.noreply.github.com>
2020-06-15 19:16:23 +0700

    API update

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-/*
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
 
 Route::post('register', 'API\RegisterController@register');
 
 Route::middleware('auth:api')->group(function(){
     Route::resource('room' , 'API\RoomController');
 });
+
+Route::get('/people' , 'API\FriendController@show');
+Route::post('/people/add/{id}', 'API\FriendController@add');
+Route::delete('/people/delete/{id}', 'API\FriendController@unfriend');
+
+
