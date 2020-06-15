abffb48a910719193fabd8f54d608c01095a993e
vedrosuwandi <43366004+vedrosuwandi@users.noreply.github.com>
2020-06-15 09:41:51 +0700

    Update navbar.blade.php

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -35,9 +35,6 @@
             <li class="nav-item">
                 <a class="nav-link" href="/people/friendlist">Friend</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Chat With Bot</a>
-            </li>
           </ul>
         </div>
         <div>
